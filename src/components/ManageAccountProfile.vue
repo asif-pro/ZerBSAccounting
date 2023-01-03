a271d000f162e1d883fad4d10c5aeb6c17aadf37
ASIF BIN ABEDIN <asif@ASIFs-MacBook-Pro.local>
2023-01-03 16:36:09 +0600

    3.01.23 4:36 PM

--- src/components/ManageAccountProfile.vue
+++ src/components/ManageAccountProfile.vue
@@ -6,7 +6,6 @@
       <br>
       <button type="submit">Insert</button>
     </form>
-    <button >Update</button>
   </div>
   <div class="" >
     <table>
@@ -23,7 +22,8 @@
           <td>{{profile.id}}</td>
           <td>{{profile.name}}</td>
           <td>{{ this.$root.transactions.filter(data => data.meta.transaction_type == 'Earning' && data.meta.transaction_profile == profile.id).reduce((acc, data) => acc + Number(data.meta.transaction_amount), 0) }}</td>
-          <td><button @click.prevent="this.$root.deleteProfile(profile.id)">Delete</button></td>
+          <td><button @click.prevent="this.$root.deleteProfile(profile.id)">Delete</button> || 
+          <button @click.prevent="">Update</button></td>
         </tr>
       </tbody>
     </table>
